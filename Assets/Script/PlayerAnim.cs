fbcf0e3cbc18fe25963bdddb7116bdfa9d0c5e2f
aa2468291 <aa2468291@gmail.com>
2019-12-03 00:11:41 +0800

    initial

---
+++ Assets/Script/PlayerAnim.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+
+public class PlayerAnim : MonoBehaviourPun
+{
+    public Animator Player_ani;
+    public bool isAction;
+    public PlayerManager playerManager;
+    public PlayerMove playerMove;
+    public GameObject Sword;
+    public bool Attacking;
+    public float Sp;
+
+    void Start()
+    {
+        Attacking = false;
+        isAction = false;
+    }
+
+
+    void Update()
+    {
+
+
+        if (photonView.IsMine)
+        {
+            if (!playerManager.IsDead)
+            {
+                if (Input.GetMouseButton(0) && Attacking == false && isAction == false)
+                {
+                    Sp = gameObject.GetComponent<PlayerManager>().PlayerSp;
+                    Debug.Log(Sp);
+                    if(Sp >= 50)
+                    {
+                        Attacking = true;
+                        isAction = true;
+                        Player_ani.SetBool("IsAttack", true);
+                        playerManager.CoSp(40);
+
+                    }
+
+
+                }
+                if (Input.GetKeyDown(KeyCode.H) )
+                {
+                   
+                    playerManager.CoHp(10);
+                   
+
+                }
+
+
+                if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+                {
+                    Player_ani.SetBool("IsWalking", true);
+                }
+                else
+                {
+                    Player_ani.SetBool("IsWalking", false);
+                }
+            }
+        }
+    }
+        public void AttackFalse()
+        {
+            Attacking = false;
+            Player_ani.SetBool("IsAttack", false);
+         }
+
+        public void AllEnd()
+         {
+             Attacking = false;
+             Player_ani.SetBool("IsAttack", false);
+             isAction = false;
+         }
+
+}
+
+
---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using Photon.Pun;
+
+
+
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+
+
+        #region IPunObservable implementation
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                //We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(PlayerHp);
+                stream.SendNext(playhp.localScale.x);
+                stream.SendNext(playhp.localScale.y);
+                stream.SendNext(playhp.localScale.z);
+        }
+            else
+            {
+                //Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.PlayerHp = (float)stream.ReceiveNext();
+                playhp.localScale = new Vector3((float)stream.ReceiveNext(), (float)stream.ReceiveNext(), (float)stream.ReceiveNext());
+        }
+        }
+
+        #endregion
+
+        #region Private Fields
+        public HpUI hpui;
+        public SpUI spui;
+        public float PlayerHp = 500;
+        public float PlayerSp = 500;
+        public bool IsDead = false;
+        public PlayerAnim playerAnim;
+        public bool IsFiring;
+        public GameObject sword;
+        public GameObject PlayerMainCamera;
+    [Tooltip("The Player's UI GameObject Prefab")]
+    [SerializeField]
+    public GameObject PlayerUiPrefab;
+    public RectTransform playhp;
+
+    #endregion
+    public void Start()
+    {
+        PlayerMainCamera.SetActive(false);
+        if (photonView.IsMine)
+            PlayerMainCamera.SetActive(true);
+
+        if (PlayerUiPrefab != null)
+        {
+            GameObject _uiGo = Instantiate(PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            playhp = GameObject.Find("PlayerUI " + photonView.Owner.NickName + photonView.ViewID).GetComponent<RectTransform>();
+        }
+        else
+        {
+            Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+        }
+
+    }
+
+        public void CoHp(float damage)
+        {
+            PlayerHp -= damage;
+            hpui.cohp(damage);
+        }
+        public void CoSp(float damage)
+        {
+            PlayerSp -= damage;
+            spui.CoSp(damage);
+            if (PlayerSp < 0)
+            {
+                PlayerSp = 0;
+            }
+        }
+
+    }
+
